a6e1b35f511a24952621a44037025c8c4fe16ea3
Maciek W <maciej.wmroblewski@gmail.com>
2017-01-04 22:57:19 +0100

    dopisanie pytanie do zadania funkcje 8.6

--- funkcje.js
+++ funkcje.js
@@ -9,3 +9,19 @@ function getTriangleArea(a,h) {
 triangle1Area = console.log(getTriangleArea(5,8));
 triangle2Area = console.log(getTriangleArea(6,2));
 triangle3Area = console.log(getTriangleArea(-1,8));
+
+//przy pierwszy commit tego projektu wyskoczyło mi takie powiadomienie. Czemu sie tak stało. Czemu to służy?
+
+/*Your name and email address were configured automatically based
+on your username and hostname. Please check that they are accurate.
+You can suppress this message by setting them explicitly:
+
+    git config --global user.name "Your Name"
+    git config --global user.email you@example.com
+
+After doing this, you may fix the identity used for this commit with:
+
+    git commit --amend --reset-author
+
+ 1 file changed, 11 insertions(+)
+*/
